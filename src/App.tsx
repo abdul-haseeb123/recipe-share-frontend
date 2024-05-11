e7347e45a355859da9bbc1ac6b1db6ba97ec28de
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-05-12 01:35:10 +0500

    setting up project again with typescript, due to shadcn

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="text-green-400">
+      App <Button>Hello world</Button>
+    </div>
+  );
+}
